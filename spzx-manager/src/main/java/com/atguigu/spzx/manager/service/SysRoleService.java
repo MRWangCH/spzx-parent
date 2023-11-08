02f3dbbf9572b1f0fda4eee0e4064e44559874e4
wangchuan <1016368304@qq.com>
2023-11-08 22:06:14 +0800

    角色管理列表页数据查询以及分页的实现

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/SysRoleService.java
@@ -0,0 +1,10 @@
+package com.atguigu.spzx.manager.service;
+
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.github.pagehelper.PageInfo;
+
+public interface SysRoleService {
+    //列表查询
+    PageInfo<SysRole> findByPage(SysRoleDto sysRoleDto, Integer current, Integer limit);
+}
---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,31 @@
+package com.atguigu.spzx.manager.service.impl;
+
+import com.atguigu.spzx.manager.mapper.SysRoleMapper;
+import com.atguigu.spzx.manager.service.SysRoleService;
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SysRoleServiceImpl implements SysRoleService {
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+
+    //查询角色列表
+    @Override
+    public PageInfo<SysRole> findByPage(SysRoleDto sysRoleDto, Integer current, Integer limit) {
+        //设置分页相关参数
+        PageHelper.startPage(current, limit);
+        //根据条件查询所有数据
+        List<SysRole> list = sysRoleMapper.findByPage(sysRoleDto);
+        //封装pageInfo对象
+        PageInfo<SysRole> pageInfo = new PageInfo<>(list);
+        return pageInfo;
+    }
+}
