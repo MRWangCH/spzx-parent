4764e60b5a8df99db700b243628a0eb9fa8513be
wangchuan <1016368304@qq.com>
2023-11-10 20:58:30 +0800

    使用Minio作为文件服务器，把固定值写入配置文件中，通过配置文件取值

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
@@ -1,5 +1,6 @@
 package com.atguigu.spzx.manager;
 
+import com.atguigu.spzx.manager.properties.MinioProperties;
 import com.atguigu.spzx.manager.properties.UserProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu.spzx"})
-@EnableConfigurationProperties(value = {UserProperties.class})
+@EnableConfigurationProperties(value = {UserProperties.class, MinioProperties.class})
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class,args);
---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/FileUploadServiceImpl.java
@@ -0,0 +1,55 @@
+package com.atguigu.spzx.manager.service.impl;
+
+import com.atguigu.spzx.common.exception.GuiguException;
+import com.atguigu.spzx.manager.properties.MinioProperties;
+import com.atguigu.spzx.manager.service.FileUploadService;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadServiceImpl implements FileUploadService {
+
+    @Autowired
+    private MinioProperties minioProperties;
+
+    @Override
+    public String upload(MultipartFile file) {
+        try {
+            // 创建minioClient 对象
+            MinioClient minioClient =
+                    MinioClient.builder()
+                            .endpoint(minioProperties.getEndpointUrl())
+                            .credentials(minioProperties.getAccessKey(), minioProperties.getSecreKey())
+                            .build();
+
+            // 创建一个bucket
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.getBucketName()).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(minioProperties.getBucketName()).build());
+            } else {
+                System.out.println("Bucket 'asiatrip' already exists.");
+            }
+            //获取上传的文件名称
+            String filename = file.getOriginalFilename();
+
+            // 文件上传
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket(minioProperties.getBucketName())
+                            .object(filename).stream(file.getInputStream(), file.getSize(), -1)
+                            .build());
+
+            //获取上传文件在minion的路径
+            String url = minioProperties.getEndpointUrl() + "/" + minioProperties.getBucketName() + "/" + filename;
+            return url;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new GuiguException(ResultCodeEnum.SYSTEM_ERROR);
+        }
+    }
+}
