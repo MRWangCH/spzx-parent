71eb4af90ce293568e0edd654d4988158c6aadaf
wangchuan <1016368304@qq.com>
2023-11-10 21:17:41 +0800

    保证上传的每个文件不重名，且按照上传的日期进行分组

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/FileUploadServiceImpl.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/FileUploadServiceImpl.java
@@ -1,5 +1,7 @@
 package com.atguigu.spzx.manager.service.impl;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.UUID;
 import com.atguigu.spzx.common.exception.GuiguException;
 import com.atguigu.spzx.manager.properties.MinioProperties;
 import com.atguigu.spzx.manager.service.FileUploadService;
@@ -11,6 +13,8 @@ import io.minio.PutObjectArgs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
 
 @Service
 public class FileUploadServiceImpl implements FileUploadService {
@@ -36,7 +40,11 @@ public class FileUploadServiceImpl implements FileUploadService {
                 System.out.println("Bucket 'asiatrip' already exists.");
             }
             //获取上传的文件名称
-            String filename = file.getOriginalFilename();
+            //1 保证每个上传文件不重名
+            //2 根据文件上传日期对文件进行分组
+            String dateDir = DateUtil.format(new Date(), "yyyyMMdd");
+            String uuid = UUID.randomUUID().toString().replaceAll("-","");
+            String filename = dateDir + "/" + uuid + file.getOriginalFilename();
 
             // 文件上传
             minioClient.putObject(
