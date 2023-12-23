06d97f02fa0c42104b2d4ac19d9ae2f5e96a05f8
wangchuan <1016368304@qq.com>
2023-12-23 15:38:43 +0800

    由于远程调用，order的token不能传到cart中，编写拦截器传递token

--- spzx-service/service-order/src/main/java/com/atguigu/spzx/order/OrderApplication.java
+++ spzx-service/service-order/src/main/java/com/atguigu/spzx/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.atguigu.spzx.order;
 
+import com.atguigu.spzx.common.anno.EnableUserTokenFeignInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = {
         "com.atguigu.spzx.feign.cart"
 })
+@EnableUserTokenFeignInterceptor
 public class OrderApplication {
 
     public static void main(String[] args) {
