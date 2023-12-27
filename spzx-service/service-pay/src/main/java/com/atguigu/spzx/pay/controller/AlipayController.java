462020533e4b5bd640f273418c57f6d0b797d149
wangchuan <1016368304@qq.com>
2023-12-27 21:18:19 +0800

    订单支付，引入阿里支付,调用阿里支付返回支付页

--- spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/controller/AlipayController.java
+++ spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/controller/AlipayController.java
@@ -1,7 +1,26 @@
 package com.atguigu.spzx.pay.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.atguigu.spzx.pay.service.AlipayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
+@RequestMapping(value = "/api/order/alipay")
 public class AlipayController {
+
+    @Autowired
+    private AlipayService alipayService;
+
+    @Operation(summary = "支付宝下单")
+    @GetMapping("/submitAlipay/{orderNo}")
+    @ResponseBody
+    public Result submitAlipay(@Parameter(name = "orderNo", description = "订单号", required = true) @PathVariable(value = "orderNo") String orderNo) {
+        String form = alipayService.submitAlipay(orderNo);
+        return Result.build(form, ResultCodeEnum.SUCCESS);
+    }
 }
--- spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/service/AlipayService.java
+++ spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/service/AlipayService.java
@@ -1,4 +1,6 @@
 package com.atguigu.spzx.pay.service;
 
 public interface AlipayService {
+    //支付宝下单
+    String submitAlipay(String orderNo);
 }
