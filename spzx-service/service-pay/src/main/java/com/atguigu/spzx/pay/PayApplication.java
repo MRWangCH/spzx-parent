8e8448b7c4d9fa1b2e7fa3ccc2681433d56e6d75
wangchuan <1016368304@qq.com>
2023-12-27 20:44:24 +0800

    订单支付，引入阿里支付

--- spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/PayApplication.java
+++ spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/PayApplication.java
@@ -1,13 +1,16 @@
 package com.atguigu.spzx.pay;
 
 import com.atguigu.spzx.common.anno.EnableUserLoginAuthInterceptor;
+import com.atguigu.spzx.pay.utils.AlipayProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableUserLoginAuthInterceptor
 @EnableFeignClients(basePackages = "com.atguigu.spzx")
+@EnableConfigurationProperties(value = {AlipayProperties.class})
 public class PayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PayApplication.class);
